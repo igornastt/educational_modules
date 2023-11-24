cc9a279c4fada3c66b2ad0838dd0db49efd906dd
igornast <grezle00@gmail.com>
2023-11-24 15:19:31 +0300

    Добавлены разбивка на страницы и классы разрешений

---
+++ users/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsModeratorPermission(BasePermission):
+    def has_permission(self, request, view):
+        if "moderator" in request.user.roles:
+            return True
+        return False
+
+
+class IsTeacherPermission(BasePermission):
+    def has_permission(self, request, view):
+
+        if "teacher" in request.user.roles:
+            return True
+        return False
+
+
+class IsStudentPermission(BasePermission):
+    def has_permission(self, request, view):
+
+        if "student" in request.user.roles:
+            return True
+        return False
