49316fa1a6eddd87f9e1d7c705f4c602fc3d83d1
igornast <grezle00@gmail.com>
2023-11-24 15:11:32 +0300

    Создано управление пакетами python и два файла python: csu.py и cut.py

---
+++ users/management/commands/cut.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='test-1@sky.pro',
+            first_name='test-1',
+            last_name='test-1',
+            is_staff=False,
+            is_superuser=False,
+            is_active=False,
+        )
+
+        user.set_password('12345678')
+        user.save()
